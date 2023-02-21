f70c3f147c4bcf940e97dcab7407233ed5b4a30b
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-02-21 19:28:21 +0300

    homework_№5_Two-level test architecture was made

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,19 +3,17 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper {
-
-    private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void goToGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 
     public void goToNewContactPage() {
-        wd.get("http://localhost/addressbook/");
+        click(By.linkText("http://localhost/addressbook/"));
     }
 }
