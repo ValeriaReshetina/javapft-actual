37c5ec0cb0d364d74715a08dbd88fe33347a2ca1
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-03-11 16:07:33 +0300

    homework_№9_In tests have been implemented list comparison

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
+    @Test (enabled = false)
     public void testNewContactCreation() throws Exception {
         String groupName = "test1";
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -7,7 +7,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
-    @Test
+    @Test (enabled = false)
     public void testContactModification() {
 
         if (!app.getContactHelper().isThereAContact()) {
